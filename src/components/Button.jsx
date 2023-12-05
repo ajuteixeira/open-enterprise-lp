aa53abe2d284d7f823ec10896b9cd161e1c4cfc9
Juliana Teixeira <teixeirajuuh@gmail.com>
2023-12-05 20:52:42 -0300

    ajuste

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,19 @@ export default function Button(props) {
   return (
     <button
       className={`
-      ${props.color === "green" && "bg-[#71A894] text-white font-medium"}
-      ${props.color === "black" && "bg-[#303031] text-white font-medium"}
-      ${props.type === "default" && "px-5  py-5 tracking-tight rounded-lg"}
+      ${
+        props.color === "green" && "bg-[#71A894] text-lg text-white font-medium"
+      }
+      ${
+        props.color === "black" && "bg-[#303031] text-lg text-white font-medium"
+      }
+      ${
+        props.type === "default" &&
+        "px-5  py-5 text-lg tracking-tight rounded-lg"
+      }
       ${
         props.type === "small" &&
-        "px-5 py-3 font-mono tracking-tighter rounded-lg"
+        "px-5 text-lg py-3 font-mono tracking-tighter rounded-lg"
       }
 `}
     >

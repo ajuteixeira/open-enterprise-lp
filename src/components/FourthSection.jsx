0b4f293ff70b38e032af5adb4d330e4d699ab4dd
Juliana Teixeira <teixeirajuuh@gmail.com>
2024-01-18 16:33:12 -0300

    ajustes de responsividade para telas xl

--- src/components/FourthSection.jsx
+++ src/components/FourthSection.jsx
@@ -7,7 +7,7 @@ import Button from "./Button";
 export default function FourthSection() {
   return (
     <Container>
-      <div className="px-24 pb-7 flex flex-col gap-3 text-center">
+      <div className="px-24 pb-7 flex flex-col gap-3 text-center xl:pb-14 xl:gap-7">
         <Title type="secondary">Help us improve Open Enterprise</Title>
         <Text>
           As an Open Enterprise ourselves, we are actively looking for new
@@ -16,59 +16,59 @@ export default function FourthSection() {
           in our success.
         </Text>
       </div>
-      <div className="flex gap-5 justify-between">
+      <div className="flex gap-5 justify-between xl:gap-10">
         <Card type="default" color="white" align="left">
-          <div className="flex flex-col justify-start gap-4">
-            <span className="font-mono font-bold text-base sm:text-sm lg:text-base">
+          <div className="flex flex-col justify-start xl:gap-7">
+            <span className="font-mono font-bold text-base sm:text-sm lg:text-base xl:text-lg">
               Ideate a list of features based on the Sociocracy...
             </span>
-            <span className="font-mono text-xs lg:text-sm text-[#303031]">
+            <span className="font-mono text-xs lg:text-sm xl:text-base text-[#303031]">
               We're looking for someone interested in business theory and
               research that'll help us bridge the gap between our...
             </span>
             <div className="w-full flex justify-between items-center sm:gap-9">
-              <span className="px-3 py-1 text-sm font-mono font-medium tracking-tighter rounded-full text-white bg-[#71A894] sm:text-xs sm:text-center lg:text-sm">
+              <span className="px-3 py-1 text-sm font-mono font-medium tracking-tighter rounded-full text-white bg-[#71A894] sm:text-xs sm:text-center lg:text-sm xl:text-base">
                 5 AUT
               </span>
-              <span className="font-mono font-bold text-base sm:text-xs lg:text-sm">
+              <span className="font-mono font-bold text-base sm:text-xs lg:text-sm xl:text-base">
                 Apply by May 15
               </span>
             </div>
           </div>
         </Card>
         <Card type="default" color="white" align="left">
-          <div className="flex flex-col justify-start gap-4">
-            <span className="font-mono font-bold text-base sm:text-sm lg:text-base">
+          <div className="flex flex-col justify-start gap-4 xl:gap-7">
+            <span className="font-mono font-bold text-base sm:text-sm lg:text-base xl:text-lg">
               Update documentation with FAQs
             </span>
-            <span className="font-mono text-xs lg:text-sm text-[#303031]">
+            <span className="font-mono text-xs lg:text-sm xl:text-base text-[#303031]">
               Review our community chat for the most frequently asked questions
               and document answers for our product docs.
             </span>
             <div className="w-full flex justify-between items-center sm:gap-9">
-              <span className="px-3 py-1 text-sm font-mono font-medium tracking-tighter rounded-full text-white bg-[#71A894] sm:text-xs sm:text-center lg:text-sm">
+              <span className="px-3 py-1 text-sm font-mono font-medium tracking-tighter rounded-full text-white bg-[#71A894] sm:text-xs sm:text-center lg:text-sm xl:text-base">
                 10 AUT
               </span>
-              <span className="font-mono font-bold text-base sm:text-xs lg:text-sm">
+              <span className="font-mono font-bold text-base sm:text-xs lg:text-sm xl:text-base">
                 Apply by May 18
               </span>
             </div>
           </div>
         </Card>
         <Card type="default" color="white" align="left">
-          <div className="flex flex-col justify-start gap-4">
-            <span className="font-mono font-bold text-base sm:text-sm lg:text-base">
+          <div className="flex flex-col justify-start gap-4 xl:gap-7">
+            <span className="font-mono font-bold text-base sm:text-sm lg:text-base xl:text-lg">
               Tutorial video series
             </span>
-            <span className="font-mono text-xs lg:text-sm text-[#303031]">
+            <span className="font-mono text-xs lg:text-sm xl:text-base text-[#303031]">
               Create a series of video tutorials that cover everything from
               start to finish on using Open Enterprise.
             </span>
             <div className="w-full flex justify-between items-center sm:gap-9">
-              <span className="px-3 py-1 text-sm font-mono font-medium tracking-tighter rounded-full text-white bg-[#71A894] sm:text-xs sm:text-center lg:text-sm">
+              <span className="px-3 py-1 text-sm font-mono font-medium tracking-tighter rounded-full text-white bg-[#71A894] sm:text-xs sm:text-center lg:text-sm xl:text-base">
                 40 AUT
               </span>
-              <span className="font-mono font-bold text-base sm:text-xs lg:text-sm">
+              <span className="font-mono font-bold text-base sm:text-xs lg:text-sm xl:text-base">
                 Apply by May 20
               </span>
             </div>
